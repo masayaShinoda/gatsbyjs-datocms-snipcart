bccad3fb2d01327b7dc26cc08fe772d03c1a14b8
masayaShinoda <masayashida@gmail.com>
2020-08-27 23:07:28 +0700

    I figured out night mode!!!

--- src/components/header.js
+++ src/components/header.js
@@ -7,29 +7,118 @@ import Styles from "../styles/Header.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMoon } from "@fortawesome/free-solid-svg-icons"
 
-const goDark = () => {
-  const parent = document.getElementById("parent")
-  parent.classList.toggle("dark")
-}
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      darkMode: false,
+    }
+    this.goDark = this.goDark.bind(this)
+    this.goLight = this.goLight.bind(this)
+  }
 
-const Header = () => (
-  <header className={Styles.Header}>
-    <div className={Styles.headerContent}>
-      <div className={Styles.logoContainer}>
-        <Link to="/">
-          <p className={Styles.logoTitle}></p>
-        </Link>
-        <button onClick={goDark}>
-          <FontAwesomeIcon
-            title="Lights on/off"
-            icon={faMoon}
-            style={{ fontSize: `2.5rem`, marginLeft: `2.5vmin` }}
-          />
-        </button>
-      </div>
-      <Nav />
-    </div>
-  </header>
-)
+  // goDark() {
+  //   const parent = document.getElementById("parent")
+  //   // console.log(this.state.darkMode)
+  //   if (this.state.darkMode === false) {
+  //     parent.classList.toggle("dark")
+  //     this.setState(() => {
+  //       return {
+  //         darkMode: true,
+  //       }
+  //     })
+  //     var mode = true
+  //     console.log("before click its light")
+  //   }
+  //   if (this.state.darkMode === true) {
+  //     parent.classList.toggle("dark")
+  //     this.setState(() => {
+  //       return {
+  //         darkMode: false,
+  //       }
+  //     })
+  //     var mode = false
+  //     console.log("before click its dark")
+  //   }
+
+  //   // var mode = this.state.darkMode
+  //   localStorage.setItem("mode", JSON.stringify(mode))
+  //   console.log(
+  //     "the value being stored is " + JSON.parse(localStorage.getItem("mode"))
+  //   )
+  // }
+
+  goDark() {
+    const parent = document.getElementById("parent")
+    parent.classList.add("dark")
+    this.setState(() => {
+      return {
+        darkMode: true,
+      }
+    })
+    console.log("go dark!")
+    var mode = true
+    localStorage.setItem("mode", JSON.stringify(mode))
+  }
+
+  goLight() {
+    const parent = document.getElementById("parent")
+    parent.classList.remove("dark")
+    this.setState(() => {
+      return {
+        darkMode: false,
+      }
+    })
+    console.log("go light!")
+    var mode = false
+    localStorage.setItem("mode", JSON.stringify(mode))
+  }
+
+  componentDidMount() {
+    // console.log(JSON.parse(localStorage.getItem("mode")))
+    const storedData = JSON.parse(localStorage.getItem("mode"))
+    console.log(
+      "the state of storeddata is " + JSON.stringify(this.state.darkMode)
+    )
+    this.setState(() => {
+      return {
+        darkMode: storedData,
+      }
+    })
+    if (storedData === true) {
+      const parent = document.getElementById("parent")
+      parent.classList.add("dark")
+    }
+    console.log(
+      "the state of darkmode is " + JSON.stringify(this.state.darkMode)
+    )
+  }
+
+  render() {
+    return (
+      <header className={Styles.Header}>
+        <div className={Styles.headerContent}>
+          <div className={Styles.logoContainer}>
+            <Link to="/">
+              <p className={Styles.logoTitle}></p>
+            </Link>
+            <button
+              onClick={
+                this.state.darkMode === false ? this.goDark : this.goLight
+              }
+            >
+              <FontAwesomeIcon
+                title="Lights on/off"
+                icon={faMoon}
+                style={{ fontSize: `2.5rem`, marginLeft: `2.5vmin` }}
+              />
+            </button>
+          </div>
+          <Nav />
+        </div>
+      </header>
+    )
+  }
+}
 
 export default Header
