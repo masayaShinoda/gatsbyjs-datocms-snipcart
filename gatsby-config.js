dee80cde74193adad1a1b5574d7a629f09cb707d
Masaya Shida <masayashida@gmail.com>
2020-12-27 18:57:27 +0700

    added fontawesome fix plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
         autopop: true,
       },
     },
+    `gatsby-plugin-fontawesome-css`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
